6657ccbaaa67a5cb45d177bc7276dd58ca53127d
Anenokil <anenokil.w@gmail.com>
2023-01-15 10:35:02 +0500

    Move all themes to special file

--- main.py
+++ main.py
@@ -11,12 +11,13 @@ else:
 import urllib.request as urllib2  # Для проверки наличия обновлений
 import wget  # Для загрузки обновления
 import zipfile  # Для распаковки обновления
+from themes import *  # Мой модуль с темами
 
 """ Информация о программе """
 
 PROGRAM_NAME = 'Dictionary'
-PROGRAM_VERSION = 'v7.0.0_PRE-71'
-PROGRAM_DATE = '15.1.2023  10:21 (UTC+5)'
+PROGRAM_VERSION = 'v7.0.0_PRE-72'
+PROGRAM_DATE = '15.1.2023  10:34 (UTC+5)'
 
 """ Папки и файлы """
 
@@ -45,38 +46,6 @@ if SAVES_DIR not in os.listdir(RESOURCES_DIR):
     os.mkdir(SAVES_PATH)
 if LOCAL_SETTINGS_DIR not in os.listdir(RESOURCES_DIR):
     os.mkdir(LOCAL_SETTINGS_PATH)
-
-""" Темы """
-
-THEMES = ('light', 'dark', 'infernal', 'solar')
-
-# Все: bg
-# Все, кроме frame: fg
-# Все, кроме текста: border
-# Frame: relief
-# Кнопки: activebackground
-# Entry: selectbackground, highlightcolor
-
-ST_BG          = {THEMES[0]: '#EEEEEE', THEMES[1]: '#222222', THEMES[2]: '#DD1515', THEMES[3]: '#FFFFDD'}  # Цвет фона окна
-ST_BG_FIELDS   = {THEMES[0]: '#FFFFFF', THEMES[1]: '#171717', THEMES[2]: '#FFAAAA', THEMES[3]: '#EEEECC'}  # Цвет фона полей ввода
-
-ST_BORDER      = {THEMES[0]: '#222222', THEMES[1]: '#111111', THEMES[2]: '#330000', THEMES[3]: '#444422'}  # Цвет рамок
-ST_RELIEF      = {THEMES[0]: 'groove',  THEMES[1]: 'solid',   THEMES[2]: 'groove',  THEMES[3]: 'groove' }  # Стиль рамок
-
-ST_SELECT      = {THEMES[0]: '#AABBBB', THEMES[1]: '#444444', THEMES[2]: '#FF5500', THEMES[3]: '#CCCCAA'}  # Цвет выделения текста
-ST_HIGHLIGHT   = {THEMES[0]: '#00DD00', THEMES[1]: '#007700', THEMES[2]: '#EEEEEE', THEMES[3]: '#22DD00'}  #
-
-ST_BTN         = {THEMES[0]: '#D0D0D0', THEMES[1]: '#202020', THEMES[2]: '#DD2020', THEMES[3]: '#E0E0C0'}  # Цвет фона обычных кнопок
-ST_BTN_SELECT  = {THEMES[0]: '#BABABA', THEMES[1]: '#272727', THEMES[2]: '#DD5020', THEMES[3]: '#CBCBA9'}  # Цвет фона обычных кнопок при нажатии
-ST_BTNY        = {THEMES[0]: '#88DD88', THEMES[1]: '#446F44', THEMES[2]: '#CC6633', THEMES[3]: '#AAEE88'}  # Цвет фона да-кнопок
-ST_BTNY_SELECT = {THEMES[0]: '#77CC77', THEMES[1]: '#558055', THEMES[2]: '#CC9633', THEMES[3]: '#99DD77'}  # Цвет фона да-кнопок при нажатии
-ST_BTNN        = {THEMES[0]: '#FF6666', THEMES[1]: '#803333', THEMES[2]: '#CD0000', THEMES[3]: '#FF6644'}  # Цвет фона нет-кнопок
-ST_BTNN_SELECT = {THEMES[0]: '#EE5555', THEMES[1]: '#904444', THEMES[2]: '#CD3000', THEMES[3]: '#EE5533'}  # Цвет фона нет-кнопок при нажатии
-
-ST_FG_TEXT     = {THEMES[0]: '#222222', THEMES[1]: '#979797', THEMES[2]: '#000000', THEMES[3]: '#444422'}  # Цвет обычного текста
-ST_FG_LOGO     = {THEMES[0]: '#FF7200', THEMES[1]: '#803600', THEMES[2]: '#FF7200', THEMES[3]: '#FF8800'}  # Цвет текста логотипа
-ST_FG_FOOTER   = {THEMES[0]: '#666666', THEMES[1]: '#666666', THEMES[2]: '#222222', THEMES[3]: '#666644'}  # Цвет текста нижнего колонтитула
-ST_FG_WARN     = {THEMES[0]: '#DD2222', THEMES[1]: '#AA0000', THEMES[2]: '#FF9999', THEMES[3]: '#EE4400'}  # Цвет текста нижнего колонтитула
 
 """ Другое """
 
@@ -3188,16 +3157,16 @@ class SettingsW(tk.Toplevel):
         self.lbl_dct_name = tk.Label(self, text=f'Открыт словарь "{dct_savename}"', bg=ST_BG[th], fg=ST_FG_TEXT[th])
         self.tabs.add(self.tab_local, text='Настройки словаря')
         # {
-        self.frame_MGSP = tk.LabelFrame(self.tab_local, bg=ST_BG[th], highlightbackground=ST_BORDER[th],
+        self.frame_mgsp = tk.LabelFrame(self.tab_local, bg=ST_BG[th], highlightbackground=ST_BORDER[th],
                                         relief=ST_RELIEF[th])
         # { {
-        self.lbl_MGSP = tk.Label(self.frame_MGSP, text='Минимальный приемлемый процент угадываний слова:',
+        self.lbl_mgsp = tk.Label(self.frame_mgsp, text='Минимальный приемлемый процент угадываний слова:',
                                  bg=ST_BG[th], fg=ST_FG_TEXT[th])
-        self.entry_MGSP = tk.Entry(self.frame_MGSP, textvariable=self.var_mgsp, width=5, relief='solid',
+        self.entry_mgsp = tk.Entry(self.frame_mgsp, textvariable=self.var_mgsp, width=5, relief='solid',
                                    validate='key', vcmd=self.vcmd, bg=ST_BG_FIELDS[th], fg=ST_FG_TEXT[th],
                                    highlightbackground=ST_BORDER[th], highlightcolor=ST_HIGHLIGHT[th],
                                    selectbackground=ST_SELECT[th])
-        self.lbl_MGSP_2 = tk.Label(self.frame_MGSP, text='Статьи, у которых процент угадывания ниже этого значения,'
+        self.lbl_mgsp_2 = tk.Label(self.frame_mgsp, text='Статьи, у которых процент угадывания ниже этого значения,'
                                                          'будут считаться более сложными',
                                    bg=ST_BG[th], fg=ST_FG_TEXT[th])
         # } }
@@ -3261,11 +3230,11 @@ class SettingsW(tk.Toplevel):
         self.lbl_dct_name.grid(row=0, padx=6, pady=(6, 0))
         self.tabs.grid(        row=1, padx=6, pady=(0, 6))
         #
-        self.frame_MGSP.grid(row=0, padx=6, pady=6, sticky='E')
+        self.frame_mgsp.grid(row=0, padx=6, pady=6, sticky='E')
         # {
-        self.lbl_MGSP.grid(  row=0, column=0,     padx=(6, 1), pady=6,      sticky='E')
-        self.entry_MGSP.grid(row=0, column=1,     padx=(0, 1), pady=6,      sticky='W')
-        self.lbl_MGSP_2.grid(row=1, columnspan=2, padx=6,      pady=(0, 6))
+        self.lbl_mgsp.grid(  row=0, column=0,     padx=(6, 1), pady=6,      sticky='E')
+        self.entry_mgsp.grid(row=0, column=1,     padx=(0, 1), pady=6,      sticky='W')
+        self.lbl_mgsp_2.grid(row=1, columnspan=2, padx=6,      pady=(0, 6))
         # }
         self.btn_forms.grid(     row=1, padx=6, pady=(0, 3))
         self.lbl_forms_warn.grid(row=2, padx=6, pady=(0, 6))
---
+++ themes.py
@@ -0,0 +1,47 @@
+""" Темы """
+
+THEMES = ('light', 'dark', 'infernal', 'solar')
+
+# Все: bg
+# Все, кроме frame: fg
+# Все, кроме текста: border
+# Frame: relief
+# Кнопки: activebackground
+# Entry: selectbackground, highlightcolor
+
+ST_BG          = {THEMES[0]: '#EEEEEE', THEMES[1]: '#222222',
+                  THEMES[2]: '#DD1515', THEMES[3]: '#FFFFDD'}  # Цвет фона окна
+ST_BG_FIELDS   = {THEMES[0]: '#FFFFFF', THEMES[1]: '#171717',
+                  THEMES[2]: '#FFAAAA', THEMES[3]: '#EEEECC'}  # Цвет фона полей ввода
+
+ST_BORDER      = {THEMES[0]: '#222222', THEMES[1]: '#111111',
+                  THEMES[2]: '#330000', THEMES[3]: '#444422'}  # Цвет рамок
+ST_RELIEF      = {THEMES[0]: 'groove',  THEMES[1]: 'solid',
+                  THEMES[2]: 'groove',  THEMES[3]: 'groove' }  # Стиль рамок
+
+ST_SELECT      = {THEMES[0]: '#AABBBB', THEMES[1]: '#444444',
+                  THEMES[2]: '#FF5500', THEMES[3]: '#CCCCAA'}  # Цвет выделения текста
+ST_HIGHLIGHT   = {THEMES[0]: '#00DD00', THEMES[1]: '#007700',
+                  THEMES[2]: '#0000FF', THEMES[3]: '#22DD00'}  #
+
+ST_BTN         = {THEMES[0]: '#D0D0D0', THEMES[1]: '#202020',
+                  THEMES[2]: '#DD2020', THEMES[3]: '#E0E0C0'}  # Цвет фона обычных кнопок
+ST_BTN_SELECT  = {THEMES[0]: '#BABABA', THEMES[1]: '#272727',
+                  THEMES[2]: '#DD5020', THEMES[3]: '#CBCBA9'}  # Цвет фона обычных кнопок при нажатии
+ST_BTNY        = {THEMES[0]: '#88DD88', THEMES[1]: '#446F44',
+                  THEMES[2]: '#CC6633', THEMES[3]: '#AAEE88'}  # Цвет фона да-кнопок
+ST_BTNY_SELECT = {THEMES[0]: '#77CC77', THEMES[1]: '#558055',
+                  THEMES[2]: '#CC9633', THEMES[3]: '#99DD77'}  # Цвет фона да-кнопок при нажатии
+ST_BTNN        = {THEMES[0]: '#FF6666', THEMES[1]: '#803333',
+                  THEMES[2]: '#CD0000', THEMES[3]: '#FF6644'}  # Цвет фона нет-кнопок
+ST_BTNN_SELECT = {THEMES[0]: '#EE5555', THEMES[1]: '#904444',
+                  THEMES[2]: '#CD3000', THEMES[3]: '#EE5533'}  # Цвет фона нет-кнопок при нажатии
+
+ST_FG_TEXT     = {THEMES[0]: '#222222', THEMES[1]: '#979797',
+                  THEMES[2]: '#000000', THEMES[3]: '#444422'}  # Цвет обычного текста
+ST_FG_LOGO     = {THEMES[0]: '#FF7200', THEMES[1]: '#803600',
+                  THEMES[2]: '#FF7200', THEMES[3]: '#FF8800'}  # Цвет текста логотипа
+ST_FG_FOOTER   = {THEMES[0]: '#666666', THEMES[1]: '#666666',
+                  THEMES[2]: '#222222', THEMES[3]: '#666644'}  # Цвет текста нижнего колонтитула
+ST_FG_WARN     = {THEMES[0]: '#DD2222', THEMES[1]: '#AA0000',
+                  THEMES[2]: '#FF9999', THEMES[3]: '#EE4400'}  # Цвет текста нижнего колонтитула
