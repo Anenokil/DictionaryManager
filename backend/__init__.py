66ef1e2972ccb755428c2fa7305ba8c28cb7d907
Anenokil <anenokil.w@gmail.com>
2025-11-19 23:36:23 +0300

    refactor: rename module learning.py to trainer.py

--- backend/__init__.py
+++ backend/__init__.py
@@ -8,7 +8,7 @@ from .types import *
 from .entry import Entry, Translations, Forms, Phrases
 from .dictionary import Dictionary
 from .manager import Manager
-from .learning import (
+from .trainer import (
     Trainer, TrainingMethod, TrainingOrder, EntrySelection,
     FormSelection, TrainingConfig, create_training_config,
 )
